52e3aa1a8f566f7bdca5949f329e6b3e1bf4b639
kuksu <kuksu95@hanmail.net>
2020-01-27 06:51:12 +0900

    #4.4

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { gql } from "apollo-boost";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "../Styles/GlobalStyles";
+import Theme from "../Styles/Theme";
+import Router from "./Router";
+import { useQuery } from "react-apollo-hooks";
 
-function App() {
-  return (
-    <div className="App"></div>
-  );
-}
+const QUERY = gql`
+  {
+    isLoggedIn @client
+  }
+`;
 
-export default App;
+export default () => {
+
+  const { data: { isLoggedIn } } = useQuery(QUERY);
+
+  return (    
+    <ThemeProvider theme={ Theme }>
+      <>
+        <GlobalStyles />
+        <Router isLoggedIn={isLoggedIn} />
+      </>
+    </ThemeProvider>
+  )
+};
